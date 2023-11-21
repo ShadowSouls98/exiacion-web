0031e78eed4111528f746f0b6088f60801351648
ShadowSoul98 <89111519+ShadowSoul98@users.noreply.github.com>
2023-11-20 19:01:44 -0600

    firt commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const user = {
+  name: 'Ivan Caldera',
+  username: 'Shadow',
+  avatar: 'https://imgs.search.brave.com/GEARYcqawKF0aprmBvQtS7bkOBtuO2q5xbdn6yu--FM/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9mcmFz/ZXNwYXJhZm90b3Mu/dG9wL3dwLWNvbnRl/bnQvdXBsb2Fkcy8y/MDIzLzAxL2NoaWNv/cy1mb3RvLXBlcmZp/bC5qcGc'
+}
+function getName(user){
+  return `${user.name} (${user.username})`
+}
+function getGreeting(user){
+  if(user){
+    return <h1>Bienvenido a Expiacion {getName(user)}</h1>
+  }
+  return <h1>Bienvenido a Expiacion ser desconocido</h1>
+}
+const name = "shadow"
+const element = (
+  <div>
+    <h1>{getGreeting(user)}</h1>
+    <img src={user.avatar} />
+  </div>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+) 
+const container = document.getElementById('root')
+
+ReactDOM.render(element, container)
